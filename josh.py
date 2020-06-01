9a5854654de07934fa4fe67c5a2759d5dbdd5996
wepl <wepl123@yahoo.com>
2020-06-01 18:52:37 -0400

    fuck

--- josh.py
+++ josh.py
@@ -1,3 +1,26 @@
-for i in range(23):
-    print("josh is coodl")
-print("msansdgsdf")
+tweet = "Hello, World! This is a messageto"
+tweetLen = len( tweet )
+
+punc = [".", ",", "?", "!", ";", ":", "/", "\'", "\""]
+def isPunc( textChar ):
+  return textChar in punc
+
+
+def stringToWordList( words ):
+  wordList = []
+  word = None
+
+  for x in range( tweetLen ):
+    char = tweet[x]
+    
+    if not isPunc( char ) and char != " ": 
+      word = char if word == None else word + char
+      
+    if char == " " or x + 1 == tweetLen:
+      wordList.append( word.lower() )
+      word = None
+  
+  return wordList
+
+
+print( stringToWordList( tweet ) )
