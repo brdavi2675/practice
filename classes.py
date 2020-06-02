7537f39c9dd2343ed8dcd00ac606881fb7c20a52
wepl <wepl123@yahoo.com>
2020-06-01 21:11:10 -0400

    owned

---
+++ classes.py
@@ -0,0 +1,19 @@
+class testClass:
+    def __init__( self, name, age ):
+        self.name = name
+        self.age = age
+
+    
+    
+fuck = testClass("josh", 10)
+print( fuck )
+print( fuck.name )
+print( fuck.age )
+
+
+
+
+
+
+
+
